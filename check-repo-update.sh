4387ee2e4599311ad4b1baa72f77f15c24ccb943
Your Name <cjj8168@gmail.com>
2022-01-22 00:06:27 +0800

    cjj/add price change

--- check-repo-update.sh
+++ check-repo-update.sh
@@ -1,8 +1,7 @@
 changed=0
 git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
 if [ $changed = 1 ]; then
-    git pull
-    echo "Updated successfully";
+    echo "Not up to date";
 else
     echo "Up-to-date"
 fi
